76b6cc185ba499eaf39851af26a2e80b483f300a
Andrew Martin <andrewcm@utexas.edu>
2018-04-02 20:03:08 -0500

    Working on storing files in AI interface

--- Motif/Controllers/AIPianoViewController.swift
+++ Motif/Controllers/AIPianoViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
-import PianoView
-import MusicTheorySwift
-import AudioKit
+//import PianoView
+//import MusicTheorySwift
+//import AudioKit
 import WebKit
+import AVFoundation
+
+struct MIDIClip {
+    let midiData: Data
+    let creator: String
+    let timestamp: Date
+}
+
 
 class AIPianoViewController: UIViewController, WKUIDelegate, WKScriptMessageHandler {
     
@@ -18,9 +26,12 @@ class AIPianoViewController: UIViewController, WKUIDelegate, WKScriptMessageHand
     var webView: WKWebView!
     
     @IBOutlet weak var placeholderView: UIView!
+    var midiPlayer: AVMIDIPlayer?
     
     let eventNames = ["user", "ai"]
     var eventFunctions : Dictionary<String, (String)->Void> = Dictionary<String, (String)->Void>()
+    
+    var sessionClips: [MIDIClip] = []
     
     override func loadView() {
         super.loadView()
@@ -28,9 +39,23 @@ class AIPianoViewController: UIViewController, WKUIDelegate, WKScriptMessageHand
         let controller = WKUserContentController()
         webConfiguration.userContentController = controller
         
+        eventFunctions["user"] = {(body) in
+            if body.starts(with: "[77,84,") {
+                let bytes = body.dropFirst().dropLast().split(separator: ",").map {UInt8($0)!}
+                let clip = MIDIClip(midiData: Data(bytes:bytes), creator: "user", timestamp: Date())
+                self.sessionClips.append(clip)
+            }
+        }
+        
+        eventFunctions["ai"] = {(body) in
+            if body.starts(with: "TVRoZ") {
+                let clip = MIDIClip(midiData: Data(base64Encoded: body)!, creator: "ai", timestamp: Date())
+                self.sessionClips.append(clip)
+            }
+        }
+        
         for eventname in eventNames {
             controller.add(self, name: eventname)
-            eventFunctions[eventname] = {(body) in print(body)}
         }
         
         webView = WKWebView(frame: .zero, configuration: webConfiguration)
@@ -57,7 +82,10 @@ class AIPianoViewController: UIViewController, WKUIDelegate, WKScriptMessageHand
                             if (self.readyState == 4) {
                                 console.log(self.response);
                                 var base64 = btoa(new Uint8Array(self.response).reduce((data, byte) => data + String.fromCharCode(byte), ''));
-                                window.webkit.messageHandlers.ai.postMessage(base64);
+                                if (base64.startsWith('TVRoZ')) { // Standard MIDI file format header
+                                    window.webkit.messageHandlers.ai.postMessage(base64);
+                                }
+                                
                             }
                         }
                         this.addEventListener("readystatechange", onReadyStateChange, false);
@@ -69,6 +97,26 @@ class AIPianoViewController: UIViewController, WKUIDelegate, WKScriptMessageHand
         let button = UIButton(frame: CGRect(x: 320, y: 20, width:30, height:24))
         button.setImage(UIImage(named: "microphone"), for: .normal)
         
+    }
+    
+
+    func createAVMIDIPlayerFromMIDIFIleDLS() {
+        
+        let midiString = ""
+        var midiFileURL: URL!
+        
+        guard let bankURL = Bundle.main.url(forResource: "gs_soundfont", withExtension: "sf2" ) else {
+            fatalError("\"gs_soundfont.sf2\" file not found.")
+        }
+        
+        do {
+            try self.midiPlayer = AVMIDIPlayer(contentsOf: midiFileURL, soundBankURL: bankURL)
+            print("created midi player with sound bank url \(bankURL)")
+        } catch let error as NSError {
+            print("Error \(error.localizedDescription)")
+        }
+        
+        self.midiPlayer?.prepareToPlay()
     }
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
