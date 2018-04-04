37783477a9f099f9b17eef7e9a4c731984f6b2fa
Michael Asper <asper@utexas.edu>
2018-04-04 12:42:31 -0500

    customized side bar

--- Motif/Controllers/ClipLibraryViewController.swift
+++ Motif/Controllers/ClipLibraryViewController.swift
@@ -144,14 +144,8 @@ class ClipLibraryViewController: UIViewController, UITableViewDelegate, UITableV
         toggle = !toggle
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func logOut() {
+        
     }
-    */
 
 }
